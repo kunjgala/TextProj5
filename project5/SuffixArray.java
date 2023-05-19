0b60422a3d17405bd264d10dee512a5c84abfb22
Kunj Gala <kunjgala15@gmail.com>
2023-05-19 13:47:32 -0700

    first commit

---
+++ project5/SuffixArray.java
@@ -0,0 +1,81 @@
+package project5;
+import java.util.*;
+
+public class SuffixArray {
+
+  public static class Suffix implements Comparable<Suffix> {
+    int suffixIndex;
+    int suffixRank;
+    int nextRank;
+ 
+    public Suffix(int index, int rank, int nextRank) {
+      suffixIndex = index;
+      suffixRank = rank;
+      this.nextRank = nextRank;
+    }
+         
+    public int compareTo(Suffix s) {
+      if (suffixRank != s.suffixRank)
+        return Integer.compare(suffixRank, s.suffixRank);
+      return Integer.compare(nextRank, s.nextRank);
+    }
+  }
+  
+  public static ArrayList<Integer> createSuffixArray(String inputString) {
+    int n = inputString.length();
+    Suffix[] suffixes = new Suffix[n];
+         
+    for (int i = 0; i < n; i++) {
+      suffixes[i] = new Suffix(i, inputString.charAt(i) - '$', 0);
+    }
+    for (int i = 0; i < n; i++)
+      suffixes[i].nextRank = (i + 1 < n ? suffixes[i + 1].suffixRank : -1);
+ 
+    Arrays.sort(suffixes);
+  
+    int[] indexes = new int[n];
+
+    for (int length = 4; length < 2 * n; length <<= 1) {
+      updateSuffixRanks(suffixes, indexes, length);
+      updateNextRanks(suffixes, indexes, n, length);
+      Arrays.sort(suffixes);
+    }
+    
+    ArrayList<Integer> suffixArray = new ArrayList<>();
+    for (int i = 0; i < n; i++)
+      suffixArray.add(suffixes[i].suffixIndex);
+ 
+    return suffixArray;
+  }   
+
+  private static void updateSuffixRanks(Suffix[] suffixes, int[] indexes, int length) {
+    int rank = 0;
+    int prev = suffixes[0].suffixRank;
+    suffixes[0].suffixRank = rank;
+    indexes[suffixes[0].suffixIndex] = 0;
+    
+    for (int i = 1; i < suffixes.length; i++) {
+      if (suffixes[i].suffixRank == prev && suffixes[i].nextRank == suffixes[i - 1].nextRank) {
+        prev = suffixes[i].suffixRank;
+        suffixes[i].suffixRank = rank;
+      } else {
+        prev = suffixes[i].suffixRank;
+        suffixes[i].suffixRank = ++rank;
+      }
+      indexes[suffixes[i].suffixIndex] = i;
+    }
+  }
+
+  private static void updateNextRanks(Suffix[] suffixes, int[] indexes, int n, int length) {
+    for (int i = 0; i < n; i++) {
+      int nextPosition = suffixes[i].suffixIndex + length / 2;
+      suffixes[i].nextRank = nextPosition < n ? suffixes[indexes[nextPosition]].suffixRank : -1;
+    }
+  }
+
+    
+  public ArrayList<Integer> construct(String S) {
+   return createSuffixArray(S);
+  }
+
+}
